f96da5e25a4724c480143f46a8a00feedaf61c0d
Gabriel Musat Mestre <gabriel.musatmestre@datadoghq.com>
2025-04-21 11:18:20 +0200

    Revert "Bump MSRV to 1.82, toolchain to 1.85 (apache#14811)"

--- datafusion/physical-expr/src/equivalence/properties/mod.rs
+++ datafusion/physical-expr/src/equivalence/properties/mod.rs
@@ -680,7 +680,7 @@ impl EquivalenceProperties {
                         req.expr.eq(&existing.expr)
                             && req
                                 .options
-                                .is_none_or(|req_opts| req_opts == existing.options)
+                                .map_or(true, |req_opts| req_opts == existing.options)
                     },
                 )
         })
