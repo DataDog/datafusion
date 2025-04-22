bdd0c0529187c1c6d5f13817459419ac570b801e
Gabriel <45515538+gabotechs@users.noreply.github.com>
2025-04-22 10:02:30 +0200

    Revert "Bump MSRV to 1.82, toolchain to 1.85 (apache#14811)" (#19)

--- datafusion/functions-aggregate/src/first_last.rs
+++ datafusion/functions-aggregate/src/first_last.rs
@@ -571,9 +571,9 @@ where
         for (idx_in_val, group_idx) in group_indices.iter().enumerate() {
             let group_idx = *group_idx;
 
-            let passed_filter = opt_filter.is_none_or(|x| x.value(idx_in_val));
+            let passed_filter = opt_filter.map_or(true, |x| x.value(idx_in_val));
 
-            let is_set = is_set_arr.is_none_or(|x| x.value(idx_in_val));
+            let is_set = is_set_arr.map_or(true, |x| x.value(idx_in_val));
 
             if !passed_filter || !is_set {
                 continue;
