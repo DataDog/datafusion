3d0131268a3565f90b38b7e144cac26abcd0af3a
Rohan Krishnaswamy <47869999+rkrishn7@users.noreply.github.com>
2025-09-21 05:36:29 -0700

    chore: Upgrade Rust version to 1.90.0 (#17677)
    * chore: bump workspace rust version to 1.90.0
    
    * fix clippy errors
    
    * fix clippy errors
    
    * try using dedicate runner temp space
    
    * retrigger
    
    * inspect disk usage
    
    * split build/run
    
    * disable debug info in ci profile
    
    * revert ci changes
    
    (cherry picked from commit bea1b0a1ce325414ab9ed89a87df86d45f3d821c)

--- datafusion/physical-plan/src/joins/sort_merge_join/exec.rs
+++ datafusion/physical-plan/src/joins/sort_merge_join/exec.rs
@@ -356,10 +356,10 @@ impl DisplayAs for SortMergeJoinExec {
                     "SortMergeJoin: join_type={:?}, on=[{}]{}",
                     self.join_type,
                     on,
-                    self.filter.as_ref().map_or("".to_string(), |f| format!(
-                        ", filter={}",
-                        f.expression()
-                    ))
+                    self.filter.as_ref().map_or_else(
+                        || "".to_string(),
+                        |f| format!(", filter={}", f.expression())
+                    )
                 )
             }
             DisplayFormatType::TreeRender => {
