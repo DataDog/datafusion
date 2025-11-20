75ed1ad2c07fc5557acb458ba6f3944d78cb61e4
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2025-11-20 08:28:09 +0800

    Refactor state management in `HashJoinExec` and use CASE expressions for more precise filters (#18451)
    This PR is part of an EPIC to push down hash table references from
    HashJoinExec into scans. The EPIC is tracked in
    https://github.com/apache/datafusion/issues/17171.
    
    A "target state" is tracked in
    https://github.com/apache/datafusion/pull/18393.
    There is a series of PRs to get us to this target state in smaller more
    reviewable changes that are still valuable on their own:
    - https://github.com/apache/datafusion/pull/18448
    - https://github.com/apache/datafusion/pull/18449 (depends on
    https://github.com/apache/datafusion/pull/18448)
    - (This PR): https://github.com/apache/datafusion/pull/18451
    
    This PR refactors state management in HashJoinExec to make filter
    pushdown more efficient and prepare for pushing down membership tests.
    
    - Refactor internal data structures to clean up state management and
    make usage more idiomatic (use `Option` instead of comparing integers,
    etc.)
    - Uses CASE expressions to evaluate pushed-down filters selectively by
    partition Example: `CASE hash_repartition % N WHEN partition_id THEN
    condition ELSE false END`
    
    ---------
    
    Co-authored-by: Lía Adriana <lia.castaneda@datadoghq.com>
    (cherry picked from commit 5b0aa37c8562d141c6c9e0a026115b4e6b905ca2)
    (cherry picked from commit e9d1985f3e9a3c0f14f0663099bd892f758a24cf)

--- datafusion/core/tests/physical_optimizer/filter_pushdown/util.rs
+++ datafusion/core/tests/physical_optimizer/filter_pushdown/util.rs
@@ -64,6 +64,9 @@ impl FileOpener for TestOpener {
         _file: PartitionedFile,
     ) -> Result<FileOpenFuture> {
         let mut batches = self.batches.clone();
+        if self.batches.is_empty() {
+            return Ok((async { Ok(TestStream::new(vec![]).boxed()) }).boxed());
+        }
         if let Some(batch_size) = self.batch_size {
             let batch = concat_batches(&batches[0].schema(), &batches)?;
             let mut new_batches = Vec::new();
@@ -334,11 +337,12 @@ impl TestStream {
     /// least one entry in data (for the schema)
     pub fn new(data: Vec<RecordBatch>) -> Self {
         // check that there is at least one entry in data and that all batches have the same schema
-        assert!(!data.is_empty(), "data must not be empty");
-        assert!(
-            data.iter().all(|batch| batch.schema() == data[0].schema()),
-            "all batches must have the same schema"
-        );
+        if let Some(first) = data.first() {
+            assert!(
+                data.iter().all(|batch| batch.schema() == first.schema()),
+                "all batches must have the same schema"
+            );
+        }
         Self {
             data,
             ..Default::default()
