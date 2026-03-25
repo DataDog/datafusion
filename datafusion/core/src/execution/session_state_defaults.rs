bd59d7a1126f99fe0e2f334ffde497025a70d49f
gstvg <28798827+gstvg@users.noreply.github.com>
2026-03-25 02:15:28 +0000

    minor fixes

--- datafusion/core/src/execution/session_state_defaults.rs
+++ datafusion/core/src/execution/session_state_defaults.rs
@@ -115,7 +115,11 @@ impl SessionStateDefaults {
 
     /// returns the list of default [`LambdaUDF`]s
     pub fn default_lambda_functions() -> Vec<Arc<dyn LambdaUDF>> {
-        function_nested::all_default_lambda_functions()
+        #[cfg(feature = "nested_expressions")]
+        return functions_nested::all_default_lambda_functions();
+
+        #[cfg(not(feature = "nested_expressions"))]
+        return Vec::new();
     }
 
     /// returns the list of default [`AggregateUDF`]s
--- datafusion/sql/src/unparser/expr.rs
+++ datafusion/sql/src/unparser/expr.rs
@@ -546,6 +546,7 @@ impl Unparser<'_> {
                         params.iter().map(|param| param.as_str().into()).collect(),
                     ),
                     body: Box::new(self.expr_to_sql_inner(body)?),
+                    syntax: ast::LambdaSyntax::Arrow,
                 }))
             }
             Expr::LambdaVariable(l) => Ok(ast::Expr::Identifier(
